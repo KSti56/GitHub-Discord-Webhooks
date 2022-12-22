f7316c63bf4b00fa32f2fee83af7c1ffa5d25224
ThisLightMan <birdkage56@gmail.com>
2022-12-22 01:02:27 -0600

    Create getPages.js

---
+++ src/lib/getPages.js
@@ -0,0 +1,21 @@
+const MAX_LENGTH = 500
+
+const calculateCharacterLength = arr => arr.join('\n').length
+
+const getPages = (commits) => {
+    const pages = []
+    let pageIndex = 0
+    for (const commit of commits) {
+        if (!pages[pageIndex]) pages[pageIndex] = []
+
+        if (calculateCharacterLength(pages[pageIndex]) + commit.length > MAX_LENGTH) {
+            pageIndex++
+            pages[pageIndex] = [commit]
+        } else {
+            pages[pageIndex].push(commit)
+        }
+    }
+    return pages
+}
+
+module.exports = getPages
